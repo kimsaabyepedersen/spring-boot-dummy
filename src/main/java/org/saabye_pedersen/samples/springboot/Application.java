c420beb94d212ab9b91b016e001d345d96103bec
Kim Saabye Pedersen <kpeder@amazon.com>
2018-07-31 13:14:54 +0200

    First commit

---
+++ src/main/java/org/saabye_pedersen/samples/springboot/Application.java
@@ -0,0 +1,31 @@
+package org.saabye_pedersen.samples.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RestController
+    class TimeController {
+
+        @GetMapping (path = "/",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+        String getTime() {
+
+            return "{\"time\": \"" + LocalDateTime.now().toString() + "\"}";
+
+        }
+
+
+    }
+}
